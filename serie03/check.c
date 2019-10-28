8eb07cdf7e7868e2069a57d2cbe3f94d69e196e7
roctubre <robertryan.chavez@gmail.com>
2019-10-28 21:07:23 +0100

    generalize sequence sizes

--- serie03/check.c
+++ serie03/check.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define LEN_BASE   10
+#define LEN_SEARCH 3
+
 /*
     Check if a 3 digit number sequence is present in a 10 digit number sequence.
     Input:
@@ -9,28 +12,35 @@
     1 ... found
     0 ... not found
 */
-int check(int x[10], int y[3]) {
+int check(int x[LEN_BASE], int y[LEN_SEARCH]) {
     // iterate through base sequence until len(x) - len(y)
-    for (int i = 0; i <= 7; ++i) {
-        // compare sequence from x[i] to x[i + 2] with y[0] to y[2]
-        if (x[i] == y[0] && x[i + 1] == y[1] && x[i + 2] == y[2]) {
-            return 1;
+    for (int i = 0; i <= LEN_BASE - LEN_SEARCH; ++i) {
+        // compare x and y starting from x[i]
+        for (int j = 0; j < LEN_SEARCH; ++j) {
+            // leave loop if digits are not equal
+            if (x[i + j] != y[j]) {
+                continue;
+            }
+            // if the last digits are equal return 1
+            if (j == LEN_SEARCH - 1) {
+                return 1;
+            }
         }
     }
     return 0;
 }
 
 int main() {
-    int x[10];      // 10 digit number sequence
-    int y[3];       // 3 digit number sequence to look for
+    int x[LEN_BASE];      // 10 digit number sequence
+    int y[LEN_SEARCH];       // 3 digit number sequence to look for
     
     // get input for x and y
     printf("Number sequence:\n");
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < LEN_BASE; ++i) {
         printf("#%d: ", i + 1); scanf("%d", &x[i]);
     }
     printf("\nSearch sequence:\n");
-    for (int i = 0; i < 3; ++i) {
+    for (int i = 0; i < LEN_SEARCH; ++i) {
         printf("#%d: ", i + 1); scanf("%d", &y[i]);
     }
 
