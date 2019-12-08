760f08e31bc096db88d3a9bc7542716b36bdf6d0
roctubre <robertryan.chavez@gmail.com>
2019-12-08 21:15:50 +0100

    refactored, comparing characters directly

--- serie09/anagram.c
+++ serie09/anagram.c
@@ -7,9 +7,7 @@
 #define MAXLEN 256
 
 int     anagram     (char* firstStr, char* secondStr);
-int*    char2int    (char* input, int length);
-char*   int2char    (int* input, int length);
-void    quickSort   (int* x, int n);
+void    quickSort   (char* x, int n);
 
 
 int main() {
@@ -33,34 +31,16 @@ int main() {
     Returns 1 if anagram, else 0.
 */
 int anagram(char* firstStr, char* secondStr) {
-    int len1 = strlen(firstStr);                        // length of firstStr
-    int len2 = strlen(secondStr);                       // length of secondStr
-    int* int_firstStr = char2int(firstStr, len1);       // convert firstStr to decimal ASCII representation
-    int* int_secondStr = char2int(secondStr, len2);     // convert secondStr to decimal ASCII representation
-    char* sorted_firstStr = NULL;                       // sorted characters of firstStr
-    char* sorted_secondStr = NULL;                      // sorted characters of secondStr
-    int result = 0;                                     // 0 = TRUE, else FALSE
+    int len1 = strlen(firstStr);    // length of firstStr
+    int len2 = strlen(secondStr);   // length of secondStr
+    int result = 0;                 // 0 = TRUE, else FALSE
 
-    // sort int arrays
-    quickSort(int_firstStr, len1);
-    quickSort(int_secondStr, len2);
-    
-    // transform int arrays back to characters
-    sorted_firstStr = int2char(int_firstStr, len1);
-    sorted_secondStr = int2char(int_secondStr, len2);
+    // sort char arrays
+    quickSort(firstStr, strlen(firstStr));
+    quickSort(secondStr, strlen(secondStr));
 
-    // compare strings using strcmp from string.h
-    result = strcmp(sorted_firstStr, sorted_secondStr);
-
-    // cleanup
-    free(int_firstStr);
-    free(int_secondStr);
-    free(sorted_firstStr);
-    free(sorted_secondStr);
-    int_firstStr = NULL;
-    int_secondStr = NULL;
-    sorted_firstStr = NULL;
-    sorted_secondStr = NULL;
+    // compare sorted strings using strcmp from string.h
+    result = strcmp(firstStr, secondStr);
 
     // strcmp returns 0 if strings are equal, but according to the task 
     // we have to return 1 if they are anagrams of each other.
@@ -69,62 +49,29 @@ int anagram(char* firstStr, char* secondStr) {
 
 
 /*
-    Creates an integer array from a character array.
-    Characters are typecasted into their respective decimal ASCII value.
-
-    Note: Converts upper case characters to lower case.
-*/
-int* char2int(char* input, int length) {
-    int* output = malloc(length*sizeof(int));
-    int i;
-    for (i = 0; i < length; ++i) {
-        output[i] = (int)input[i];
-        if (output[i] >= 65 && output[i] <= 90) {   // check if uppercase -> change to lower case
-            output[i] += 32;
-        }
-    }
-
-    return output;
-}
-
-
-/*
-    Creates a character array from an integer array.
-    Integers are typecasted into their respective character ASCII value.
-*/
-char* int2char(int* input, int length) {
-    char* output = malloc((length+1)*sizeof(char));
-    int i;
-    for (i = 0; i < length; ++i) {
-        output[i] = (int)input[i];
-    }
-    output[i] = '\0';   // null-byte to mark end of string
-    return output;
-}
-
-
-/*
     Sorts a given vector using the quicksort algorithm (recursive function).
+    Sorting is in-place.
 */
-void quickSort(int* x, int n) {
+void quickSort(char* x, int n) {
     if (n <= 1) return;                     // only continue if element count > 1
 
     int j, k;
+    char temp;
     for (j = 0, k = 1; k < n; ++k) {
         if (x[k] < x[0]) {                  // compare to pivot, swap values
             ++j;
             if (k == j) continue;
-            x[j] = x[j] + x[k];
-            x[k] = x[j] - x[k];
-            x[j] = x[j] - x[k];
+            temp = x[j];
+            x[j] = x[k];
+            x[k] = temp;
         }
     }
 
     // swap pivot and last lesser element
     if (j > 0) {
-        x[0] = x[0] + x[j];
-        x[j] = x[0] - x[j];
-        x[0] = x[0] - x[j];
+        temp = x[j];
+        x[j] = x[0];
+        x[0] = temp;
     }
 
     quickSort(x, j++);          // sort lesser part
